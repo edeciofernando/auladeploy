a146eee68390b1b111007f9f7ad40fe8c179df02
Edecio Fernando <edeciofernando@gmail.com>
2019-10-21 22:46:39 -0300

    Updates

---
+++ precache-manifest.91b31eda77f45046a3932c1abd96f4cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1a78d683ac36d6b161cce4f801e32ac",
+    "url": "/auladeploy/index.html"
+  },
+  {
+    "revision": "d7e689ecb3b38239f410",
+    "url": "/auladeploy/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6e2b065c51d116b26f22",
+    "url": "/auladeploy/static/js/2.cb7a12ec.chunk.js"
+  },
+  {
+    "revision": "d7e689ecb3b38239f410",
+    "url": "/auladeploy/static/js/main.aebf6ae8.chunk.js"
+  },
+  {
+    "revision": "50bad5686ce9300a706a",
+    "url": "/auladeploy/static/js/runtime-main.b193b62e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/auladeploy/static/media/logo.25bf045c.svg"
+  }
+]);
